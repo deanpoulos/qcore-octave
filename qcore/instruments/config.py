2b1b0d0600e0e15483f1e60afedb3ebf18d7adce
Atharv Joshi <atharvjoshi@ymail.com>
2022-09-14 23:07:05 +0800

    Make ids str

--- qcore/instruments/config.py
+++ qcore/instruments/config.py
@@ -12,9 +12,9 @@ class InstrumentConfig(dict):
         value: list of ids corresponding to the instruments qcrew has of the given class
         """
         self[MS46522B] = ["VNA1"]
-        self[QM] = [None]
-        self[SC5503B] = [10002656]
-        self[SC5511A] = [10002657]
-        self[SA124] = ([19184645, 20234154],)
-        self[LMS] = list(range(25330, 25338))
+        self[QM] = ["None"]
+        self[SC5503B] = ["10002656"]
+        self[SC5511A] = ["10002657"]
+        self[SA124] = ["19184645", "20234154"]
+        self[LMS] = [str(id) for id in range(25330, 25338)]
         self[GS200] = ["90X823743", "91X336839"]
