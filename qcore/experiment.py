868d10280b82380488dd1b33babf2bd60a7d079c
Atharv Joshi <atharvjoshi@ymail.com>
2022-11-11 11:35:34 +0800

    Write demo script for rr spec, integrate with Experiment

--- qcore/experiment.py
+++ qcore/experiment.py
@@ -1,6 +1,13 @@
+import time
+
 from qcore.instruments import QM
 from qcore.helpers.datasaver import Datasaver
 from qcore.helpers.plotter import Plotter
+from pathlib import Path
+from datetime import datetime
+from qcore.resource import Resource
+from qcore.sweep import Sweep
+from qcore.dataset import Dataset
 
 
 class Experiment:
@@ -8,8 +15,50 @@ class Experiment:
     Abstract class for experiments using QUA sequences.
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        name: str,
+        qm: QM,
+        savefolder: Path,
+        nametag: str = "",
+        fetch_interval: int = 2,
+    ):
+        self.name = name
+        self._savefolder = savefolder
+        self._nametag = nametag
+        self._filepath = None
+
+        self.qm = qm
+        self.fetch_interval = fetch_interval
+
+    def get_filepath(self) -> Path:
+        """ """
+        if self._filepath is None:
+            date, time = datetime.now().strftime("%Y-%m-%d %H-%M-%S").split()
+            folderpath = self._savefolder / date
+            filesuffix = f"_{self._nametag}" if self._nametag else ""
+            filename = f"{time}_{self.name}{filesuffix}.h5"
+            self._filepath = folderpath / filename
+        return self._filepath
+
+    def snapshot(self) -> dict:
+        """
+        snapshot includes instance attributes that do not start with "_" are are not instances of excluded classes - Resource, Sweep, Dataset, DataSaver, LivePlotter
+        """
+
+    def get_metadata(self) -> dict:
+        """ """
+        resources = [v for v in self.__dict__.values() if isinstance(v, Resource)]
+        metadata = {resource.name: resource.snapshot() for resource in resources}
+
+        xcls = (Resource, Sweep, Dataset)  # excluded classes
+        xkeys = ("datasaver", "plotter")  # excluded keys
+        snapshot = {}
+        for k, v in self.__dict__.items():
+            if not isinstance(v, xcls) and not k.startswith("_") and not k in xkeys:
+                snapshot[k] = v
+
+        return {**metadata, None: snapshot}
 
     def init_variables(self):
         raise NotImplementedError
@@ -20,17 +69,23 @@ class Experiment:
     def construct_pulse_sequence(self):
         raise NotImplementedError
 
-    def run(self, save, plot, qm: QM):
-        """
-        save (tuple[str] | bool): True to live save all datasets, False to save no data, and tuple of strings corresponding to dataset names to save specific data.
-        plot: tuple[str] | bool: True to live plot all datasets, False to plot no data, and tuple of strings corresponding to dataset names to plot specific data.
-        """
-        if save:
-            self.datasaver = Datasaver(*save)
-        if plot:
-            self.plotter = Plotter(self.wait, *plot)
+    def process_data(self):
+        raise NotImplementedError
 
-        qm.execute(self)
+    def run(self, save: tuple = (), plot: tuple = ()) -> None:
+        """ """
+        self.datasaver = Datasaver(path=self.get_filepath(), *save)
+        self.plotter = Plotter(*plot)
 
-        while qm.is_processing():
-            ...
+        self.qm.execute(self.construct_pulse_sequence())
+
+        time.sleep(self.fetch_interval)
+
+        with self.datasaver as datasaver:
+            datasaver.save_metadata(self.get_metadata())
+            while self.qm.is_processing():
+                # fetch latest batch of partial data along with data counts
+                data, current_count, last_count = self.qm.fetch()
+                if data:  # empty dict means no new results available
+                    self.process_data(datasaver, data, current_count, last_count)
+                time.sleep(self.fetch_interval)
--- tests/test_rr_spec_exp.py
+++ tests/test_rr_spec_exp.py
@@ -6,47 +6,63 @@ from qcore.expvariable import ExpVar
 from qcore.sweep import Sweep
 from qcore.sequences.rr_spec import generate_rr_spec
 from qcore.sequences.constructors import construct_sweep
+from qcore.instruments import QM
+from qcore.dataset import Dataset
+import numpy as np
 
 
 class RR_Spec(Experiment):
-
-    def __init__(self, rr: Readout):
-        super().__init__()
+    def __init__(self, rr: Readout, **kwargs):
         self.rr = rr
-    
+
+        super().__init__(**kwargs)
+
     def init_variables(self):
 
-        self.I = ExpVar(name='I', var_type=fixed, is_adc=True)
-        self.Q = ExpVar(name='Q', var_type=fixed, is_adc=True)
-        
-        self.freq = Sweep(
-            name='freq',
-            var_type=int,
-            units='Hz',
-            start=int(-53e6),
-            stop=int(-47e6),
-            step=int(0.1e6)
-        )
+        # set control variables
+        self.wait_time = 20000  # ns
 
+        # set sweep variables
         self.N = Sweep(
-            name='N',
+            name="N",
             var_type=int,
             start=0,
             stop=50000,
             step=1,
         )
 
-        self.wait_time = 40000
-        self.arg_mapping = {
-            'freq' : self.freq.q_var
-        }
-    
+        self.freq = Sweep(
+            start=int(-60e6),
+            stop=int(-40e6),
+            step=int(0.5e6),
+            var_type=int,
+            include_endpoint=True,
+            name="freq",
+            units="Hz",
+        )
+
+        # define datasets
+        self.I = Dataset(axes=[self.N, self.freq], name="I", var_type=fixed)
+        self.Q = Dataset(axes=[self.N, self.freq], name="Q", var_type=fixed)
+
+        # for derived datasets, include initial data for calculating running total
+        init_data = np.zeros(self.freq.length)
+        self.iq_avg = Dataset(axes=[self.freq], name="IQ_AVG", data=init_data)
+        self.magnitude = Dataset(
+            axes=[self.freq], name="MAGNITUDE", units="dB", data=init_data
+        )
+        self.phase = Dataset(
+            axes=[self.freq], name="PHASE", units="rad", data=init_data
+        )
+
+        self.arg_mapping = {"freq": self.freq.q_var}
+
     def process_streams(self):
 
         self.I.process_stream()
         self.Q.process_stream()
-        self.freq.process_stream()
-        self.N.process_stream()
+        self.freq.process_stream(save_all=False)
+        self.N.process_stream(save_all=False)
 
     def construct_pulse_sequence(self):
 
@@ -61,12 +77,46 @@ class RR_Spec(Experiment):
                 pulse_sequence=rr_spec,
                 arg_mapping=self.arg_mapping,
                 wait_time=self.wait_time,
-                wait_elem=self.rr
+                wait_elem=self.rr,
             )
 
             with stream_processing:
                 self.process_streams()
-        
+
         # add with_stream context here
         return qua_program
 
+    def process_data(self, datasaver, data, current_count, last_count):
+        """this is INSIDE the fetch loop!!! use for live processing only!!!"""
+        # while saving, assume sweep order and dimension and dataset axes order and dimension is consistent
+
+        # save frequency data
+        datasaver.save_data(self.freq, data["freq"])
+
+        # save raw I and Q data at the correct position
+        pos = slice(last_count, current_count)
+        datasaver.save_data(self.I, data["I"], index=(pos, ...))
+        datasaver.save_data(self.Q, data["Q"], index=(pos, ...))
+
+        # calculate and store derived datasets for plotting
+        num_batches = current_count - last_count  # for weighted averages
+        i_avg, q_avg = np.average(data["I"], axis=0), np.average(data["Q"], axis=0)
+        iq_avg = i_avg + 1j * q_avg
+        self.iq_avg.data = (
+            last_count * self.iq_avg.data + num_batches * iq_avg
+        ) / current_count
+
+        phase = np.unwrap(np.angle(data["I"] + 1j * data["Q"]))
+        phase_avg = np.average(phase, axis=0)
+        self.phase.data = (
+            last_count * self.phase.data + num_batches * phase_avg
+        ) / current_count
+
+        magnitude = 20 * np.log10(np.abs(data["I"] + 1j * data["Q"]))
+        magnitude_avg = np.average(magnitude, axis=0)
+        self.magnitude.data = (
+            last_count * self.magnitude.data + num_batches * magnitude_avg
+        ) / current_count
+
+        # live plot datasets
+        self.plotter.plot()
