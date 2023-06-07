2aff005f6e10f0c16f894202a922c2e7a6ac7a53
Atharv Joshi <atharvjoshi@ymail.com>
2023-06-07 18:57:08 +0800

    Implement basic interactions between Server and Instrument widgets
    Server widget UI is part of verse
    
    Test round trip - connection, use, and disconnection - with DummyInstrument

--- qcore/instruments/gui/instrument_widget.py
+++ qcore/instruments/gui/instrument_widget.py
@@ -1,11 +1,9 @@
 """ """
 
-from typing import get_type_hints, Type
-
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 
-from qcore.gui.ui_instrument_widget import Ui_instrument_widget
+from qcore.instruments.gui.instrument_widget_ui import Ui_instrument_widget
 from qcore.helpers.logger import logger
 from qcore.instruments.instrument import Instrument, DummyInstrument, ConnectionError
 from qcore.instruments.drivers.vaunix_lms import LMS
@@ -14,11 +12,11 @@ from qcore.instruments.drivers.vaunix_lms import LMS
 class InstrumentWidget(qtw.QWidget):
     """ """
 
+    RO_FIELD_STYLE = "background-color: lightGray"
+
     name_edited = qtc.pyqtSignal(str)
 
-    def __init__(
-        self, instrument: Instrument, type: Type[Instrument] = None, *args, **kwargs
-    ) -> None:
+    def __init__(self, instrument: Instrument, type=None, *args, **kwargs) -> None:
         """ """
         super().__init__(*args, **kwargs)
 
@@ -27,28 +25,43 @@ class InstrumentWidget(qtw.QWidget):
 
         self.instrument = instrument
         self.instrument_type = type if type is not None else instrument.__class__
-        self.ui.type_field.setText(self.instrument_type.__name__)
-        self.ui.id_field.setText(instrument.id)
         self.setWindowTitle(f"{self.instrument_type.__name__}#{instrument.id}")
 
-        self.fields = {"name": self.ui.name_field, "id": self.ui.id_field}
+        # initialize top-level read-only fields
+        self.ui.type_field.setStyleSheet(InstrumentWidget.RO_FIELD_STYLE)
+        self.ui.id_field.setStyleSheet(InstrumentWidget.RO_FIELD_STYLE)
+        self.ui.status_field.setStyleSheet(InstrumentWidget.RO_FIELD_STYLE)
+        self.ui.type_field.setText(self.instrument_type.__name__)
+        self.ui.id_field.setText(instrument.id)
+
+        # initialize with a nominal window size
+        nominal_size = (600, 300)
+        self.resize(*nominal_size)
+        self.setMinimumSize(qtc.QSize(*nominal_size))
+
+        # disable get, set, and disconnect buttons until instrument is connected
+        self.ui.get_button.setEnabled(False)
+        self.ui.set_button.setEnabled(False)
+        self.ui.disconnect_button.setEnabled(False)
+
+        self.fields: dict[str, qtw.QLineEdit] = {
+            "id": self.ui.id_field,
+            "name": self.ui.name_field,
+            "status": self.ui.status_field,
+        }
+        self.ui.name_field.setText(instrument.name)
+        self.ui.status_field.setText("Not connected")
         self.create_parameters()
 
-        for field in self.fields.values():
-            field.returnPressed.connect(self.configure_instrument)
-
-        self.ui.name_field.textChanged.connect(self.name_edited.emit)
-        self.ui.refresh_button.clicked.connect(self.refresh_instrument)
+        self.fields["name"].textChanged.connect(self.name_edited.emit)
         self.ui.connect_button.clicked.connect(self.connect_instrument)
         self.ui.disconnect_button.clicked.connect(self.disconnect_instrument)
-
-        logger.info(f"Connected instrument '{self.instrument.name}'")
-        self.refresh_instrument()
+        self.ui.get_button.clicked.connect(self.refresh_instrument)
+        self.ui.set_button.clicked.connect(self.configure_instrument)
 
     def create_parameters(self):
         """ """
-        parameters = self.instrument_type.params
-        for name, param in parameters.items():
+        for name in self.instrument_type.params:
             if name not in self.fields:
                 label = qtw.QLabel(self.ui.scroll_area_contents)
                 label.setText(name)
@@ -58,7 +71,26 @@ class InstrumentWidget(qtw.QWidget):
                 elif name in self.instrument_type.gettable_params:
                     self.ui.gettables_form.addRow(label, field)
                     field.setReadOnly(True)
+                    field.setStyleSheet(InstrumentWidget.RO_FIELD_STYLE)
                 self.fields[name] = field
+
+    def connect_instrument(self):
+        """ """
+        try:
+            self.instrument.connect()
+        except ConnectionError as err:
+            logger.error(err)  # TODO error handling
+        else:
+            self.refresh_instrument()
+
+    def disconnect_instrument(self):
+        """ """
+        try:
+            self.instrument.disconnect()
+        except ConnectionError as err:
+            logger.error(err)  # TODO error handling
+        else:
+            self.update_status(self.instrument.status)
 
     def refresh_instrument(self):
         """ """
@@ -67,10 +99,10 @@ class InstrumentWidget(qtw.QWidget):
 
     def update_parameters(self, status: bool, **snapshot):
         """ """
-        self.update_status(status)
         for field in self.fields.values():
             field.setEnabled(status)
             field.clear()
+        self.update_status(status)
         for name, value in snapshot.items():
             self.fields[name].setText(str(value))
 
@@ -80,37 +112,19 @@ class InstrumentWidget(qtw.QWidget):
         self.ui.status_field.setText(status_text)
         self.ui.connect_button.setDisabled(status)
         self.ui.disconnect_button.setEnabled(status)
-
-    def connect_instrument(self):
-        """ """
-        try:
-            self.instrument.connect()
-        except ConnectionError as err:
-            logger.error(err)  # TODO error logging
-        else:
-            self.refresh_instrument()
-
-    def disconnect_instrument(self):
-        """ """
-        self.instrument.disconnect()
-        snapshot = {"name": self.instrument.name, "id": self.instrument.id}
-        self.update_parameters(self.instrument.status, **snapshot)
+        self.ui.get_button.setEnabled(status)
+        self.ui.set_button.setEnabled(status)
 
     def configure_instrument(self):
         """ """
-        parameters = {k: v.text() for k, v in self.fields.items() if not v.isReadOnly()}
-        # TODO GET RID OF THIS HACK WITH BETTER INPUT VALIDATION AND PARSING
-        for k, v in parameters.items():
-            if v.lower() in ("t", "true"):
-                parameters[k] = True
-            elif v.lower() in ("f", "false"):
-                parameters[k] = False
-
+        # TODO input text -> parameter type conversion
+        parameters = {k: v.text() for k, v in self.fields.items()}
         try:
             self.instrument.configure(**parameters)
         except (ConnectionError, TypeError, ValueError) as err:
             logger.error(err)
         self.refresh_instrument()
+
 
 if __name__ == "__main__":
     app = qtw.QApplication([])
