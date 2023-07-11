fb1b5a6984f71c1a4dd4d9d080d98ccc231d7cf8
Atharv Joshi <atharvjoshi@ymail.com>
2023-07-11 15:47:21 +0800

    Expose Dataset, Sweep, Stage to API

--- qcore/__init__.py
+++ qcore/__init__.py
@@ -1,6 +1,8 @@
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 from qcore.helpers.logger import logger
 from qcore.resource import Resource
 from qcore.experiment import Experiment
 import qcore.libs.qua_macros as qua
+from qcore.helpers.stage import Stage
+from qcore.variables import Dataset, Sweep
