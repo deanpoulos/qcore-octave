a96d226f640a6af5ba1031d618b6f90bbbb58aab
Atharv Joshi <atharvjoshi@ymail.com>
2023-07-12 14:44:01 +0800

    Add fft function

--- qcore/libs/data_fns.py
+++ qcore/libs/data_fns.py
@@ -11,8 +11,8 @@ def mag(data):
     return np.sqrt(x**2 + y**2)
 
 
-def phase(data, delay=None, freq=None):
-    """running average"""
+def phase(data, freq, delay=0):
+    """ """
     if freq is None:  # freq-dependent phase, freq got from incoming data at index = 0
         x, y, freq = data
     else:  # constant frequency calculation, freq to be passed in by the user
@@ -20,14 +20,12 @@ def phase(data, delay=None, freq=None):
     return np.angle(np.exp(1j * 2 * np.pi * delay * freq) * (x + 1j * y))
 
 
-"""
-def fft(prev_data, incoming_data, prev_count, incoming_count, length=None):
-    (adc_data,) = incoming_data
-    adc_data -= np.average(adc_data)
-    fft_data = np.abs(np.fft.fft(adc_data)) / length
-    fft_data = fft_data[: int(length / 2 + 1)]
-    return fft_data, ...
-"""
+def fft(data, length=1):
+    """ """
+    data = data - np.average(data, axis=1)[:, None]
+    fft_data = np.abs(np.fft.fft(data)) / length
+    return fft_data[: int(length / 2 + 1)]
+
 
 DATAFN_MAP = {
     k: v for k, v in locals().items() if not k == "isfunction" and isfunction(v)
