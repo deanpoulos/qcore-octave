ed9984e13485d3e08e8f2271bc80797ff2f1f87e
Atharv Joshi <atharvjoshi@ymail.com>
2023-07-14 15:37:09 +0800

    Fix demod data fn name error

--- qcore/libs/data_fns.py
+++ qcore/libs/data_fns.py
@@ -22,6 +22,7 @@ def phase(data, freq=None, delay=0, unwrap=True):
     phase = np.angle(np.exp(-1j * 2 * np.pi * delay * freq) * (x.avg + 1j * y.avg))
     return np.unwrap(phase) if unwrap else phase
 
+
 def fft(data, length):
     """ """
     (x,) = data
@@ -29,17 +30,19 @@ def fft(data, length):
     x = x - np.average(x)
     return (np.abs(np.fft.fft(x)) / length)[: int(length / 2 + 1)]
 
+
 def demod(data, freq, length):
     """ """
-    (x, ) = data
+    (x,) = data
     x = x.avg
     t_rel = np.linspace(0, length - 1, length)
-    signal = x * np.exp(1j * (2 * np.pi * freq * 1e-9 * t_rel + 0.0))
+    sig = x * np.exp(1j * (2 * np.pi * freq * 1e-9 * t_rel + 0.0))
     period_ns = int(1 / np.abs(freq) * 1e9)
     hann = signal.hann(period_ns * 2, sym=True)
     hann = hann / np.sum(hann)
-    demod_signal = np.convolve(signal, hann, "same")
+    demod_signal = np.convolve(sig, hann, "same")
     return np.array([demod_signal.real, demod_signal.imag])
+
 
 DATAFN_MAP = {
     k: v for k, v in locals().items() if not k == "isfunction" and isfunction(v)
