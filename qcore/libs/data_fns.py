5a2ebab09123fa4213e441b8bd028bda92f28e9a
Atharv Joshi <atharvjoshi@ymail.com>
2023-07-28 20:09:03 +0800

    modify demod datafn

--- qcore/libs/data_fns.py
+++ qcore/libs/data_fns.py
@@ -3,9 +3,10 @@
 from inspect import isfunction
 
 import numpy as np
-from scipy import signal
+from scipy.signal.windows import hann
 
 # the 'data' argument must be a sequence of np arrays to be unpacked by the data_fn
+
 
 def mag(data):
     """absolute value of two inputs x and y"""
@@ -37,12 +38,14 @@ def fft(data, length):
 def demod(data, freq, length):
     """ """
     (x,) = data
+    if x.ndim > 1:  # do not calculate non 1D arrays to save time
+        return np.array([np.zeros(length), np.zeros(length)])
     t_rel = np.linspace(0, length - 1, length)
     sig = x * np.exp(1j * (2 * np.pi * freq * 1e-9 * t_rel + 0.0))
     period_ns = int(1 / np.abs(freq) * 1e9)
-    hann = signal.hann(period_ns * 2, sym=True)
-    hann = hann / np.sum(hann, axis=-1)
-    demod_signal = np.convolve(sig, hann, "same")
+    hann_ = hann(period_ns * 2, sym=True)
+    hann_ = hann_ / np.sum(hann_, axis=-1)
+    demod_signal = np.convolve(sig, hann_, "same")
     return np.array([demod_signal.real, demod_signal.imag])
 
 
